9367de5737b681e7373be6695857f35438b4dd0d
Maryam Adam <maryaada@lab2r3s14.42abudhabi.ae>
2026-02-19 15:55:15 +0400

    here we go

---
+++ fractol.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maryaada <maryaada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/19 15:53:53 by maryaada          #+#    #+#             */
+/*   Updated: 2026/02/19 15:54:59 by maryaada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FRACTOL_H
+# define	FRACTOL_H
+
+#include <unistd.h> //?
+#include <math.h>
+//include the mini library thing here
+
+
+#endif
